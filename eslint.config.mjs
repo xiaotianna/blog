e869385e8b3eb1065a51e301fb73cae7dacd6617
wifi歪f <1402772884@qq.com>
2025-03-22 15:46:11 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    "@typescript-eslint/no-explicit-any": "off",
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    }
   }
 ];
 
